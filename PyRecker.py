891c4757c493531027657642a305ad00f92251c5
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-08 13:54:13 -0600

    Add files via upload

---
+++ PyRecker.py
@@ -0,0 +1,26 @@
+import requests
+import threading
+
+url = 'request url'
+
+data = {
+ 'request': 'data'
+}
+def do_request():
+
+    while True:
+        response = requests.post(url, data=data).text
+        print(response)
+
+threads = []
+
+for i  in range(50):
+    t = threading.Thread(target=do_request)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(50):
+    threads[i].start()
+
+for i in range(50):
+    threads[i].join()
